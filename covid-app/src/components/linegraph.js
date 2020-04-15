efe22cc543fea3c479bbd4ca5be3c3b4a8759ad8
zmiles17 <zgm_striker17@hotmail.com>
2020-04-15 17:11:57 -0400

    Fixed graph issue still needs work

--- covid-app/src/components/linegraph.js
+++ covid-app/src/components/linegraph.js
@@ -1,17 +1,41 @@
 import React from "react";
-import Line from "react-chartjs-2";
+import {Line} from "react-chartjs-2";
 import $ from "axios";
 
 export default class LineGraph extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        totalCases: []
+      labels: [],
+      datasets: [
+        {
+          label: 'Total Cases US',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: []
+        }
+      ]
     };
     this.chartReference = React.createRef();
   }
 
   async componentDidMount() {
+    const currentState = this.state;
     const response = await $.get(
         "https://api.thevirustracker.com/free-api?countryTimeline=US"
       ).then(function(res) {
@@ -24,7 +48,9 @@ export default class LineGraph extends React.Component {
           console.log(err);
           return null;
       });
-      this.setState({ totalCases: response });
+      currentState.datasets[0].data = response;
+      currentState.labels = new Array(response.length);
+      this.setState({ currentState });
   }
 
   render() {
