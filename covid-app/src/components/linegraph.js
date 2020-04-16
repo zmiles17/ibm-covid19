21f8b1182b09aa1a6b80a3fc4e5b26878a98be3d
zmiles17 <zgm_striker17@hotmail.com>
2020-04-16 10:40:52 -0400

    Georgia Statistics

--- covid-app/src/components/linegraph.js
+++ covid-app/src/components/linegraph.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Line} from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 import $ from "axios";
 
 export default class LineGraph extends React.Component {
@@ -9,48 +9,109 @@ export default class LineGraph extends React.Component {
       labels: [],
       datasets: [
         {
-          label: 'Total Cases US',
+          label: "Total",
           fill: false,
           lineTension: 0.1,
-          backgroundColor: 'rgba(75,192,192,0.4)',
-          borderColor: 'rgba(75,192,192,1)',
-          borderCapStyle: 'butt',
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: "butt",
           borderDash: [],
           borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(75,192,192,1)',
-          pointBackgroundColor: '#fff',
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
           pointBorderWidth: 1,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: []
-        }
-      ]
+          data: [],
+        },
+        {
+          label: "Positive",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "green",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [],
+        },
+        {
+          label: "Negative",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "red",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [],
+        },
+      ],
     };
     this.chartReference = React.createRef();
   }
 
   async componentDidMount() {
-    const currentState = this.state;
+    const currentState = Object.assign(this.state);
     const response = await $.get(
-        "https://api.thevirustracker.com/free-api?countryTimeline=US"
-      ).then(function(res) {
-         let totalCases = [];
-         for(let val in res.data.timelineitems[0]) {
-             totalCases.push(res.data.timelineitems[0][val].total_cases);
-         }
-          return totalCases;
-      }).catch(function(err) {
-          console.log(err);
-          return null;
+      "https://covidtracking.com/api/v1/states/daily.json"
+    )
+      .then(function (res) {
+        return res.data
+          .filter((obj) => obj.state === "GA")
+          .reduce(
+            (curr, next) => {
+              curr[0].push(next.totalTestResults);
+              curr[1].push(next.positive);
+              curr[2].push(next.negative);
+              curr[3].push(
+                next.date
+                  .toString()
+                  .replace(/(\d{4})(\d{2})(\d{2})/g, "$2-$3-$1")
+              );
+              return curr;
+            },
+            [[], [], [], []]
+          );
+      })
+      .catch(function (err) {
+        console.log(err);
+        return null;
       });
-      currentState.datasets[0].data = response;
-      currentState.labels = new Array(response.length);
-      this.setState({ currentState });
+    response[0].reverse();
+    response[1].reverse();
+    response[2].reverse();
+    response[3].reverse();
+    currentState.datasets[0].data = response[0];
+    currentState.datasets[1].data = response[1];
+    currentState.datasets[2].data = response[2];
+    currentState.labels = response[3];
+    this.setState({ currentState });
   }
 
   render() {
