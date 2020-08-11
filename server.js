cfa78361f95dcdae36c8fc05315775d0bbf5a7b8
zmiles17 <zgm_striker17@hotmail.com>
2020-08-10 23:39:58 -0400

    try again

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const publicPath = path.join(__dirname, 'build');
 
 // Serve up static assets (usually on heroku)
-app.use(express.static("build"));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
   
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
